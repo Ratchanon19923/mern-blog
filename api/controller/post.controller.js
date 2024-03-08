360bca9c73f9692960ee3fd5a3c1b3cb073ac800
Nihafis Hasamoh <nihafis3603@gmail.com>
2024-03-08 10:17:35 +0700

    complete seach page

--- api/controller/post.controller.js
+++ api/controller/post.controller.js
@@ -30,22 +30,23 @@ export const getposts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
     const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
-      ...(req.query.catrgory && { catrgory: req.query.catrgory }),
+      ...(req.query.category && { category: req.query.category }),
       ...(req.query.slug && { slug: req.query.slug }),
       ...(req.query.postId && { _id: req.query.postId }),
-      ...(req.query.serchTerm && {
+      ...(req.query.searchTerm && {
         $or: [
-          { title: { $regex: req.query.seachTerm, $options: "i" } },
-          { constent: { $regex: req.query.seachTerm, $options: "i" } },
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
         ],
       }),
     })
-      .sort({ updateAt: sortDirection })
+      .sort({ updatedAt: sortDirection }) 
       .skip(startIndex)
       .limit(limit);
+  
 
     const totalPosts = await Post.countDocuments();
     const now = new Date();
---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,196 @@
+import { Button, Select, TextInput } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import PostCard from "../components/PostCard";
+
+export default function Search() {
+  const [sidebarData, setSidebarData] = useState({
+    searchTerm: "",
+    sort: "desc",
+    category: "uncategorized",
+  });
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showmore, setShowmore] = useState(false);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  console.log(sidebarData);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    const sortFromUrl = urlParams.get("sort");
+    const categoryFromUrl = urlParams.get("category");
+
+    if (searchTermFromUrl || sortFromUrl || categoryFromUrl) {
+      setSidebarData({
+        ...sidebarData,
+        searchTerm: searchTermFromUrl,
+        sort: sortFromUrl,
+        category: categoryFromUrl,
+      });
+    }
+
+    const fetchPosts = async () => {
+      setLoading(true);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/post/getposts?${searchQuery}`);
+
+      if (!res.ok) {
+        setLoading(false);
+        return;
+      }
+
+      if (res.ok) {
+        const data = await res.json();
+        setPosts(data.posts);
+        setLoading(false);
+        if (data.posts.length === 9) {
+          setShowmore(true);
+        } else {
+          setShowmore(false);
+        }
+      }
+    };
+
+    fetchPosts();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    if (e.target.id === "searchTerm") {
+      setSidebarData({ ...sidebarData, searchTerm: e.target.value });
+    }
+    if (e.target.id === "sort") {
+      const order = e.target.value || "desc";
+      setSidebarData({ ...sidebarData, sort: order });
+    }
+    if (e.target.id === "category") {
+      const category = e.target.value || "uncategorized";
+      setSidebarData({ ...sidebarData, category });
+    }
+    if (e.target.id === "") {
+      console.log(e.target.id);
+      setSidebarData({
+        ...sidebarData,
+        searchTerm: "",
+        sort: "",
+        category: "",
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("searchTerm", sidebarData.searchTerm);
+    urlParams.set("sort", sidebarData.sort);
+    urlParams.set("category", sidebarData.category);
+
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  const handleShowMore = async () => {
+    const numberOfPosts = posts.length;
+    const startIndex = numberOfPosts;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("startIndex", startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/post/getposts?${searchQuery}`);
+
+    if (!res.ok) {
+      return;
+    }
+    if (res.ok) {
+      const data = await res.json();
+      setPosts([...posts, ...data.posts]);
+      if (data.posts.length === 9) {
+        setShowmore(true);
+      } else {
+        setShowmore(false);
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="p-7 border-b md:border-r md:min-h-screen border-gray-500 ">
+        <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+          <div className="flex items-center  gap-2">
+            <label className="whitespace-nowrap font-semibold">
+              Seacrch Term:
+            </label>
+            <TextInput
+              placeholder="Search..."
+              id="searchTerm"
+              type="text"
+              value={sidebarData.searchTerm}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <label className="font-semibold">Sort:</label>
+            <Select onChange={handleChange} value={sidebarData.sort} id="sort">
+              <option value="desc">Lastest</option>
+              <option value="asc">Oldest</option>
+            </Select>
+          </div>
+          <div className="flex items-center gap-2">
+            <label className="font-semibold">Category:</label>
+            <Select
+              onChange={handleChange}
+              value={sidebarData.category}
+              id="category"
+            >
+              <option value="">No Filter</option>
+              <option value="uncategorized">Uncategorized</option>
+              <option value="reactjs">React.js</option>
+              <option value="nextjs">NextJS</option>
+              <option value="javascript">JavaScript</option>
+            </Select>
+          </div>
+          <Button
+            onClick={handleChange}
+            id="clear"
+            type="submit"
+            outline
+            gradientDuoTone="purpleToBlue"
+          >
+            Clear Filters
+          </Button>
+          <Button type="submit" outline gradientDuoTone="purpleToPink">
+            Apply Filters
+          </Button>
+        </form>
+      </div>
+      <div className="w-full">
+        <h1 className="text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5">
+          Posts results:
+        </h1>
+        <div className="p-7 flex flex-wrap gap-4">
+          {!loading && posts.length === 0 && (
+            <p className="text-xl text-gray-500">No posts found</p>
+          )}
+          {loading && <p className="text-xl text-gray-500">Loading...</p>}
+          {!loading &&
+            posts &&
+            posts.map((post) => <PostCard key={post._id} post={post} />)}
+          {showmore && (
+            <div className="w-full flex justify-center mt-5">
+              <Button
+                onClick={handleShowMore}
+                type="submit"
+                outline
+                gradientDuoTone="purpleToPink"
+              >
+                Show More
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
