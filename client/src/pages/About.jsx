652f8669e144e784b16298316fb63d5dd9c58374
Nihafis Hasamoh <nihafis3603@gmail.com>
2024-03-08 10:25:38 +0700

    add pre installation

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="max-w-xl mx-auto p-3 text-center">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Fisguxsoe' Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Fisguxsoe! We're thrilled to have you here. Our journey
+              began with a simple idea: to create a platform where people can
+              find inspiration, knowledge, and connection. Whether you're a
+              seasoned expert or just starting out, our goal is to provide you
+              with valuable content that enriches your life and sparks your
+              curiosity.
+            </p>
+
+            <p>
+              At Fisguxsoe, we believe in the power of storytelling. Through our
+              articles, we aim to share diverse perspectives, explore new ideas,
+              and shed light on topics that matter. From in-depth analysis to
+              practical tips, we strive to deliver content that informs,
+              educates, and entertains. Our team is dedicated to delivering
+              high-quality, engaging content that resonates with our readers.
+            </p>
+
+            <p>
+              But Fisguxsoe is more than just a collection of articles. It's a
+              community—a place where individuals come together to learn, grow,
+              and connect. We value your feedback and ideas, so don't hesitate
+              to reach out and share your thoughts with us. Thank you for being
+              part of our journey. Together, let's explore the endless
+              possibilities of knowledge and creativity.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
- 
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import CallToAction from "../components/CallToAction";
 
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500 ">
+        Welcome to [Your Coding Project Name]! We're excited to introduce you to
+        our innovative project that aims to revolutionize [specific area or
+        industry]. Developed by a passionate team of coders, designers, and
+        enthusiasts, [Your Coding Project Name] is the culmination of months of
+        dedication and hard work.
+      </p>
+
+      <CallToAction />
+    </div>
+  );
 }
